6c6d6a7bab87d9d6d51c10b5a91adc9de9289477
andystevenson <andystevenson@mac.com>
2022-09-20 18:23:58 +0100

    trace without headers

--- netlify/edge-functions/cookies/cookies.js
+++ netlify/edge-functions/cookies/cookies.js
@@ -3,5 +3,5 @@ export default (request, context) => {
   const destination = headers.get('sec-fetch-dest')
   const content = headers.get('content-type')
   const identity = context.cookies.get('nf_jwt')
-  context.log({ url, destination, identity, content, headers })
+  context.log({ url, destination, identity, content })
 }
