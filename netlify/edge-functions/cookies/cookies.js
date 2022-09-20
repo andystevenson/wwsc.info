422e41b5a2e2a459906739894d74054ebc4e153d
andystevenson <andystevenson@mac.com>
2022-09-20 18:14:16 +0100

    fetched the correct fetch-dest

--- netlify/edge-functions/cookies/cookies.js
+++ netlify/edge-functions/cookies/cookies.js
@@ -1,6 +1,6 @@
 export default (request, context) => {
   const { url, headers } = request
-  const destination = headers.get('sec_fetch_dest')
+  const destination = headers.get('sec-fetch-dest')
   const content = headers.get('content-type')
   const identity = context.cookies.get('nf_jwt')
   context.log({ url, destination, identity, content, headers })
