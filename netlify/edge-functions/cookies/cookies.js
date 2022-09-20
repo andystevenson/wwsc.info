683524df4fd2eceb664aa9794d9062e1ff55c8ef
andystevenson <andystevenson@mac.com>
2022-09-20 18:00:07 +0100

    honing in on auth params

--- netlify/edge-functions/cookies/cookies.js
+++ netlify/edge-functions/cookies/cookies.js
@@ -1,5 +1,7 @@
 export default (request, context) => {
-  const { url, destination } = request
+  const { url, headers } = request
+  const destination = headers.get('sec_fetch_dest')
+  const content = headers.get('content-type')
   const identity = context.cookies.get('nf_jwt')
-  context.log({ url, destination, identity, request })
+  context.log({ url, destination, identity, content })
 }
