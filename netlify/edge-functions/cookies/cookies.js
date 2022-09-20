aa8a23cab86497795ede3ec3c411f5b061a02bee
andystevenson <andystevenson@mac.com>
2022-09-20 17:28:10 +0100

    trying to see what is in request

--- netlify/edge-functions/cookies/cookies.js
+++ netlify/edge-functions/cookies/cookies.js
@@ -1,5 +1,5 @@
 export default (request, context) => {
   const { url, destination } = request
   const identity = context.cookies.get('nf_jwt')
-  context.log({ url, destination, identity })
+  context.log({ url, destination, identity, request })
 }
