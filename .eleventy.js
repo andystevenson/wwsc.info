14e6f823cd65ef62683a275e1c9f5a8142c9a53f
Andy Stevenson <andystevenson@mac.com>
2022-09-25 13:20:07 +0100

    fixed build process

--- .eleventy.js
+++ .eleventy.js
@@ -1,28 +1,14 @@
 const config = require('@andystevenson/lib/11ty')
 const shortcodes = require('./src/shortcodes/shortcodes')
 const EleventyVitePlugin = require('@11ty/eleventy-plugin-vite')
-const { ViteMinifyPlugin } = require('vite-plugin-minify')
 
 const vite = {
   tempFolderName: '.11ty-vite', // Default name of the temp folder
 
   // Defaults are shown:
   viteOptions: {
-    clearScreen: false,
-    plugins: [
-      // input https://www.npmjs.com/package/html-minifier-terser options
-      ViteMinifyPlugin({}),
-    ],
-    server: {
-      mode: 'development',
-      middlewareMode: true,
-    },
-    build: {
-      mode: 'production',
-    },
     resolve: {
       alias: {
-        '/@root': process.cwd(),
         '/@input': `${process.cwd()}/src`,
       },
     },
