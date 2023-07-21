601fc3ffcecec22bd7a92c18d073d07a2b3c87a1
Andy Stevenson <andystevenson@mac.com>
2023-07-21 12:47:15 +0100

    added headers

--- netlify/functions/get-card/get-card.mjs
+++ netlify/functions/get-card/get-card.mjs
@@ -8,5 +8,9 @@ export const handler = async (event) => {
   return {
     statusCode: 200,
     body: JSON.stringify(result),
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Content-Type': 'application/json',
+    },
   }
 }
